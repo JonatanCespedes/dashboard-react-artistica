0cb596fd9f06fe613b18821949b2e9c0d4da96db
JonatanCespedes <retratos.jonatan@gmail.com>
2023-05-23 20:45:32 -0300

    First commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+    <>
+      {/* <!-- Footer --> */}
+      <footer className="sticky-footer bg-white">
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+            <span>Copyright &copy; Dashboard 2021</span>
+          </div>
+        </div>
+      </footer>
+      {/* <!-- End of Footer --> */}
+    </>
+  );
+};
