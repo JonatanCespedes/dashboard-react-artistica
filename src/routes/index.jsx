de396bac11894b582f7a19e9b74f642f4de684c3
JonatanCespedes <retratos.jonatan@gmail.com>
2023-05-30 20:59:38 -0300

    Agrega vista de productos

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
 import { Auth } from "../pages/Auth";
 import { NotFound } from "../pages/NotFound";
+import { ProductList } from "../pages/ProductsList";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/products" element={<ProductList />} />
       <Route path="/login" element={<Auth />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
