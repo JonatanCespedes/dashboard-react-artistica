e6ebc362a6b6647be5c1b280b17a83bcfa99ad31
JonatanCespedes <retratos.jonatan@gmail.com>
2023-06-07 19:25:22 -0300

    Agrega componentes Signin Signup

--- src/pages/Auth/index.jsx
+++
@@ -1,7 +0,0 @@
-export const Auth = () => {
-    return (
-        <div>
-            <h1>Auth</h1>
-        </div>
-    )
-}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
-import { Auth } from "../pages/Auth";
 import { NotFound } from "../pages/NotFound";
 import { ProductList } from "../pages/ProductsList";
+import SignIn from "../pages/User/SignIn";
+import SignUp from "../pages/User/SignUp";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/products" element={<ProductList />} />
-      <Route path="/login" element={<Auth />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
